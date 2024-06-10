559c4b58824492e954ed0655f700aa5658b72eff
AVANADE-CORP\antonio.gomez.guzman <antonio.gomez.guzman@avanade.com>
2024-06-10 12:20:59 +0200

    Adding Form Group to the contact information

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,28 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css']
 })
 export class EditContactComponent implements OnInit {
-  firstName = new FormControl();
-  lastName = new FormControl();
-  dateOfBirth = new FormControl();
-  favoritesRanking = new FormControl();
+  contactForm = new FormGroup({
+    firstName : new FormControl(),
+    lastName : new FormControl(),
+    dateOfBirth : new FormControl(),
+    favoritesRanking : new FormControl()  
+  });
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+     private contactService:ContactsService
+    ) { }
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
     if (!contactId) return
+
+    this.contactService.getContact(contactId).subscribe((contact) => {
+      if(!contact) return;
+
+      this.contactForm.controls.firstName.setValue(contact.firstName);
+      this.contactForm.controls.lastName.setValue(contact.lastName);
+      this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth);
+      this.contactForm.controls.favoritesRanking.setValue(contact.favoritesRanking);
+    })
   }
 
   saveContact() {
-    console.log(this.firstName.value);
-    console.log(this.lastName.value);
-    console.log(this.dateOfBirth.value);
-    console.log(this.favoritesRanking.value);
+    console.log(this.contactForm.controls.firstName.value);
+    console.log(this.contactForm.controls.lastName.value);
+    console.log(this.contactForm.controls.dateOfBirth.value);
+    console.log(this.contactForm.controls.favoritesRanking.value);
   }
 }
