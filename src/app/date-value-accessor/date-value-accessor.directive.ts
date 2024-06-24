749d84d497fb5ee61759042cb0f394fe2cf37585
AVANADE-CORP\antonio.gomez.guzman <antonio.gomez.guzman@avanade.com>
2024-06-24 09:18:46 +0200

    Custom DateValueAccessor: Implementing writeValue()

--- src/app/date-value-accessor/date-value-accessor.directive.ts
+++ src/app/date-value-accessor/date-value-accessor.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const DATE_VALUE_PROVIDER: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(()=> DateValueAccessorDirective),
+  multi: true, 
+};
 
 @Directive({
-  selector: '[conDateValueAccessor]'
+  selector: 'input([type=date])[formControlName],input([type=date])[formControl],input([type=date])[ngModel]'
 })
 export class DateValueAccessorDirective {
 
-  constructor() { }
+  constructor(private element: ElementRef) { }
+
+  writeValue(newValue:any){
+    if(newValue instanceof Date)
+      {
+        //yyyy-mm-dd
+        this.element.nativeElement.value = newValue.toISOString().split('T')[0];
+        //yyyy-mm-ddThh:mm:ss.000Z -> yyyy-mm-dd
+      }
+  }
 
 }
